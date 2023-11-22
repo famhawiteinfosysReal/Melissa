b5ac09348739e970788de1569d933f91b4910ef1
Nicky Lalrochhara <nickylalrochhara917@gmail.com>
2023-11-22 22:55:55 +0530

    Update preinstall.sh

--- preinstall.sh
+++ preinstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ ! -z "$famhawiteinfosysReal_ACCESS_TOKEN" ]]; then
-    echo "Downloading famhawiteinfosysReal_kit..."
-    poetry run pip install git+https://${famhawiteinfosysReal_ACCESS_TOKEN}@github.com/famhawiteinfosysReal/famhawiteinfosysReal_kit@v0.1.3
+if [[ ! -z "$USERBOTINDO_ACCESS_TOKEN" ]]; then
+    echo "Downloading userbotindo_kit..."
+    poetry run pip install git+https://${USERBOTINDO_ACCESS_TOKEN}@github.com/userbotindo/userbotindo_kit@v0.1.3
 fi
